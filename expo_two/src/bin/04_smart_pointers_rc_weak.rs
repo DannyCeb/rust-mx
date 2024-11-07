1d35191fd63cd7a49458363fab6b54d8a831f26e
Daniel Ceballos <daniel.ceballos.arvizu@gmail.com>
2024-11-06 23:16:23 -0600

    rc and weak

---
+++ expo_two/src/bin/04_smart_pointers_rc_weak.rs
@@ -0,0 +1,29 @@
+use std::rc::Rc;
+
+fn main() {
+    let valor = "Un valor".to_string();
+
+    let r_valor = Rc::new(valor);
+
+    let r_valor2 = Rc::clone(&r_valor);
+    let r_valor3 = r_valor.clone();
+
+    println!(
+        "¿Los valores de los apuntadores son los mismos? \n¿ {:p} == {:p} ? {}",
+        r_valor2.as_ptr(),
+        r_valor3.as_ptr(),
+        std::ptr::eq(r_valor2.as_ptr(), r_valor3.as_ptr())
+    );
+
+    let weak_1 = Rc::downgrade(&r_valor);
+
+    println!("¿Sigue vivo? {:p}", r_valor.as_ptr());
+
+    println!("¿Weak apunta a lo mismo? {:?}", weak_1.as_ptr());
+    //drop(r_valor);
+    //drop(r_valor2);
+    //drop(r_valor3);
+    unsafe {
+        println!("¿Qué contiene el weak? {:?}", *weak_1.into_raw());
+    }
+}
