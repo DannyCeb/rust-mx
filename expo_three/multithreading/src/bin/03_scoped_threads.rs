1b824874691eb40a00361b0745b63ad014ab232a
Daniel Ceballos <daniel.ceballos.arvizu@gmail.com>
2024-11-18 21:17:34 -0600

    fix

--- expo_three/multithreading/src/bin/03_scoped_threads.rs
+++ expo_three/multithreading/src/bin/03_scoped_threads.rs
@@ -1,4 +1,23 @@
 use std::thread;
+
+///
+/// La principal razon para usar scoped threads es indicarle al compilador que nuestros hilos no van a vivir más que los objetos fuera del scope de los threads
+///
+
+// Los spawns toman ownership por defecto
+fn main() {
+    let numbers = vec![1, 2, 3];
+
+    thread::spawn(move || {
+        // se quiere de la palabra move para que el closure tome ownership del vector
+        println!("longitud: {}", numbers.len());
+    })
+    .join()
+    .unwrap();
+
+    //println!("numbers: {:?}", numbers); // el valor ha sido movido
+}
+
 /*
 fn main() {
     let numbers = vec![1, 2, 3];
@@ -14,13 +33,3 @@ fn main() {
 }
 
 */
-
-fn main() {
-    let numbers = vec![1, 2, 3];
-
-    thread::spawn(|| {
-        println!("longitud: {}", numbers.len());
-    });
-
-    println!("numbers: {:?}", numbers);
-}
